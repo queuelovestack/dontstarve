345f18f59d6a1bdd99e79c84c06ffd3dba056c77
周新 <charleszhou@123u.com>
2018-12-04 14:06:04 +0800

    [v0.0.3]配置服务器mod

---
+++ dedicated_server_mods_setup.lua
@@ -0,0 +1,22 @@
+--There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+
+--ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+	--ServerModSetup("350811795")
+
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+	--ServerModCollectionSetup("379114180")
+
+ServerModSetup("347079953")
+ServerModSetup("362175979")
+ServerModSetup("375859599")
+ServerModSetup("378160973")
+ServerModSetup("462434129")
+ServerModSetup("466732225")
+ServerModSetup("666155465")
+ServerModSetup("758532836")
+
+--ServerModCollectionSetup("id")
